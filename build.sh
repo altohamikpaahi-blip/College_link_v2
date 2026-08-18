d4e07674fb978d24f698de0f8150bb57a920e039
altohamikpaahi-blip <altohamikpaahi@gmail.com>
2026-08-18 07:30:32 +0200

    Add password reset command for user 'eltohami'
    Added a command to reset the password for user 'eltohami' in the database.

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ set -o errexit
 # تثبيت المكتبات على السيرفر
 pip install -r requirements.txt
 
-# تجميع ملفات الاستايل والثوابت
+# تجميع وتخزين ملفات الاستايل والثوابت لـ Tailwind CSS
 python manage.py collectstatic --no-input
 
-# ترحيل وتأكيد قاعدة البيانات على السيرفر
+# ترحيل وتأكيد جداول قاعدة البيانات السحابية
 python manage.py migrate
+
+# إعادة تعيين كلمة المرور للمستخدم altohami تلقائياً وتحديثها في قاعدة البيانات
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='eltohami'); u.set_password('univ_pass_2026'); u.save(); print('تمت إعادة تعيين كلمة المرور بنجاح')"
